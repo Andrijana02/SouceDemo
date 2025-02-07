dbe96b4ae27001e3c277f5d1e413541264afceb3
Andrijana02 <andrijanailievska016@gmail.com>
2025-02-07 23:33:52 +0100

    comment

--- src/test/java/soucedemotests/CheckoutCompleteTest.java
+++ src/test/java/soucedemotests/CheckoutCompleteTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import pages.*;
 
 import static org.junit.Assert.assertEquals;
-
+//comment
 public class CheckoutCompleteTest {
     private WebDriver driver;
     private LoginPage loginPage;
